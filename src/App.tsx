5560095c471259fe984036f7807819f31ca8ee7b
Zarzula <diis.thiago@gmail.com>
2026-01-04 13:42:32 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Encyclopedia from "./pages/Encyclopedia";
+import Capsulas from "./pages/Capsulas";
+import Niveles from "./pages/Niveles";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,6 +20,8 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/enciclopedia" element={<Encyclopedia />} />
+          <Route path="/capsulas" element={<Capsulas />} />
+          <Route path="/niveles" element={<Niveles />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
